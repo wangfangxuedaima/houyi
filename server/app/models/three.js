ebed0d161cf8bd6d9a6f85bf1121d98fe44f0464
wangfang <836051006@qq.com>
2020-03-10 10:38:53 +0800

    add

--- server/app/models/three.js
+++ server/app/models/three.js
@@ -4,43 +4,22 @@ const dayjs = require("dayjs");
 const csv2mysql = require("csv2mysql");
 const path = require("path");
 const dbUtils = require("../../db");
+const csvtojson = require("csvtojson");
 const csvPath = path.resolve(__dirname, "../csv/three.csv");
 console.log("开始时间" + dayjs().format("YYYYMMDD-hh:mm:ss"));
-// for (let i = 0; i < 30; i++) {
-//   let writeSteam = fs.createWriteStream(csvPath);
-//   superagent
-//     .get("http://pim.coltorti.it:8080/csvExport/export/allProducts.csv")
-//     .auth("vip", "05mDw01F")
-//     .pipe(writeSteam)
-//     .on("finish", async function() {
-//       console.log(dayjs().format("YYYYMMDD-hh:mm:ss") + "导出csv完成");
-//       let sql = `truncate table three`;
-//       await dbUtils.ruohuaPool(sql);
-//       csv2mysql(
-//         {
-//           host: "123.56.228.134",
-//           port: "3306",
-//           user: "root",
-//           password: "rh20200109",
-//           database: "ruohua",
-//           table: "three",
-//           csv: {
-//             delimiter: ";"
-//           }
-//         },
-//         csvPath
-//       )
-//         .then(function() {
-//           console.log("写入数据库成功");
-//         })
-//         .catch((error) => {
-//           console.log(error);
-//         });
-//     })
-//     .on("error", (err) => {
-//       console.log("读取", err.stack);
-//     });
-// }
-
-// let sql = `truncate table three`;
-// dbUtils.ruohuaPool(sql);
+let sql = `truncate table three_wf`;
+dbUtils.ruohuaPool(sql);
+fs.createReadStream(csvPath)
+  .pipe(
+    csvtojson({
+      delimiter: ";"
+    })
+  )
+  .subscribe((json) => {
+    console.log(json.Categories);
+    let sql = `INSERT INTO three_wf (Image, Image2) VALUES ('${json.Categories}', '${json["Product Url"]}')`;
+    dbUtils.ruohuaPool(sql);
+  })
+  .on("done", () => {
+    console.log("完成");
+  });
